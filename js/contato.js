e1df738ca2326d628561a480bb696b75b1ea7c22
ivanalves1 <ivan.ats060806@gmail.com>
2025-05-29 16:26:41 -0300

    pagina de contato finalizado com js

---
+++ js/contato.js
@@ -0,0 +1,11 @@
+function enviado() {
+    alert("Mensagem enviada com sucesso!");
+    window.location.href = "../index.html";
+    return false;
+}
+
+function cancelado() {
+    alert("Mensagem cancelada!");
+    window.location.href = "../index.html";
+    return false;
+}
